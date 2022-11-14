ccf218fc7dddf37ead3260a20fa80d39ce6c4e27
papaYasha <nikitayarosh350@gmail.com>
2022-11-14 13:39:21 +0300

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by student on 14.11.22.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
