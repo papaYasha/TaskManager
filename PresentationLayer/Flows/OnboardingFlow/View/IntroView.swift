bd36a69c03bf1d4c29ec18a98b6a892a03a87d24
papaYasha <nikitayarosh350@gmail.com>
2022-12-01 16:45:41 +0300

    start work w/ first onboarding screen, create custom animation that lowers the main scene

--- PresentationLayer/Flows/OnboardingFlow/View/IntroView.swift
+++ PresentationLayer/Flows/OnboardingFlow/View/IntroView.swift
@@ -1,8 +1,89 @@
 import SwiftUI
 
 struct IntroView: View {
+    
+    @State var showWalkthroughPage: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BG")
+                .ignoresSafeArea()
+            
+            navBar()
+
+            introScreen()
+            
+        }
+        .animation(.interactiveSpring(response: 1.1, dampingFraction: 0.85), value: showWalkthroughPage)
+    }
+    
+    @ViewBuilder
+    func navBar() -> some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.left")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("Black"))
+            }
+            
+            Spacer()
+            
+            Button("Skip") {
+                
+            }
+            .font(.custom(Constants.sansRegular, size: 14))
+            .foregroundColor(Color("Black"))
+        }
+        .padding(.horizontal, 15)
+        .padding(.top, 10)
+        .frame(maxWidth: .infinity, alignment: .top)
+    }
+    
+    @ViewBuilder
+    func introScreen() -> some View {
+        GeometryReader {
+            let size = $0.size
+            
+            VStack(spacing: 10) {
+                Image("man1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: size.width, height: size.height / 2)
+                    .padding(.top, 80)
+                
+                Text("Clearhead")
+                    .font(.custom(Constants.sansBold, size: 27))
+                    .offset(y: -55)
+                
+                Text(Constants.loremIpsum)
+                    .font(.custom(Constants.sansRegular, size: 14))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30)
+                    .offset(y: -50)
+                
+                Text("Let's Begin")
+                    .font(.custom(Constants.sansSemiBold, size: 14))
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 14)
+                    .foregroundColor(.white)
+                    .background {
+                        Capsule()
+                            .fill(Color("Black"))
+                    }
+                    .onTapGesture {
+                        showWalkthroughPage.toggle()
+                    }
+                
+                    .padding(.top, -25)
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .offset(y: showWalkthroughPage ? -size.height : 0)
+        }
+        .ignoresSafeArea()
     }
 }
 
